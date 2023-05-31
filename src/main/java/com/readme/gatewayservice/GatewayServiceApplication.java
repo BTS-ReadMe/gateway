2fa8352e75f27749dff007222d4cc1f79e6f0cab
Ju-Yeongmin <drodudals1@gmail.com>
2023-05-31 17:39:41 +0900

    [UPDATE] gateway AuthorizationFilter cors 수정
    - gateway AuthorizationFilter cors 설정 수정

--- src/main/java/com/readme/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/readme/gatewayservice/GatewayServiceApplication.java
@@ -3,6 +3,11 @@ package com.readme.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +17,24 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedOrigin("https://readme.life");
+		corsConfiguration.addAllowedOrigin("http://localhost:3000");
+		corsConfiguration.addAllowedOrigin("http://43.200.189.164");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.setAllowCredentials(true);
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		return source;
+	}
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		return new CorsWebFilter(corsConfigurationSource());
+	}
 
 }
